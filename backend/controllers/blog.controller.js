e6411fb520e09571ed381b596975d9ae5cab7add
Anurag <anurag.2201043cs@iiitbh.ac.in>
2025-06-01 20:33:39 +0530

    backend updated

--- backend/controllers/blog.controller.js
+++ backend/controllers/blog.controller.js
@@ -58,3 +58,28 @@ export const updateBlog = async (req, res) => {
         res.status(500).json({ success: false, message: "Error updating blog", error: error.message });
     }
 };
+
+export const getOwnBlogs = async(req, res) =>{
+    try {
+        const userId = req.id;
+        if(!userId){
+            return res.status(400).json({
+                message:"User ID is required"
+            })
+        }
+        const blogs = await Blog.find({author:userId}).populate({
+            path:"author",
+            select:'firstName lastName photoUrl'
+        })
+        if(!blogs){
+            return res.status(404).json({message:"No blogs found", blogs:[], success:false})
+        }
+        return res.status(200).json({blogs, success:true})
+    } catch (error) {
+        return res.status(500).json({
+            message:"Error fetching blogs",
+            error:error.message
+        })
+    }
+}
+

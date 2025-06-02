ea56e863640b707be9bd8bb7b9bfa8fa92d86dce
Anurag <anurag.2201043cs@iiitbh.ac.in>
2025-06-02 19:54:19 +0530

    contoller for blogs updated

--- backend/controllers/blog.controller.js
+++ backend/controllers/blog.controller.js
@@ -106,3 +106,112 @@ export const deleteBlog = async(req, res) =>{
     }
 }
 
+export const getPublishedBlog = async (_, res) =>{
+    try {
+        const blogs = await Blog.find({isPublished:true}).sort({createdAt: -1}).populate({path:"author", select:"firstName lastName photoUrl"})
+        if(!blogs){
+            return res.status(401).json({
+                message:"Blogs not found"
+            })
+        }
+        return res.status(200).json({
+            success:true,
+            blogs,
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message:"Failed to get published blogs"
+        })
+    }
+}
+
+export const togglePublishBlog = async (req, res)=>{
+    try {
+        const {blogId} = req.params;
+        // const {publish} = req.query; // true/false
+
+        const blog = await Blog.findById(blogId);
+        if(!blog){
+            return res.status(404).json({
+                message:"Blog not found"
+            })
+        }
+        // publish status based on the query parameter
+        blog.isPublished = !blog.isPublished
+        await blog.save()
+
+        const statusMessage = blog.isPublished ? "Published" : "Unpublished"
+        return res.status(200).json({
+            success:true,
+            message:`Blog is ${statusMessage}`
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message:"Failed to update status"
+        })
+    }
+}
+
+export const likeBlog = async (req, res) =>{
+    try {
+        const blogId = req.params.id;
+        const likeKrneWaleUserKiId = req.id;
+        const blog = await Blog.findById(blogId).populate({path:'likes'});
+        if(!blog){
+            return res.status(404).json({
+                message:"Blog not found",
+                success:false
+            })
+
+        }
+        // like logic started
+        await blog.updateOne({$addToSet: {likes: likeKrneWaleUserKiId}})
+        await blog.save()
+        return res.status(200).json({message:"Blog liked", blog, success:true})
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+
+export const dislikeBlog = async (req, res) =>{
+    try {
+        const blogId = req.params.id;
+        const likeKrneWaleUserKiId = req.id;
+        const blog = await Blog.findById(blogId);
+        if(!blog){
+            return res.status(404).json({
+                message:"Blog not found",
+                success:false
+            })
+
+        }
+        //dislike logic started
+        await blog.updateOne({$pull: {likes: likeKrneWaleUserKiId}})
+        await blog.save()
+        return res.status(200).json({message:"Blog disliked", blog, success:true})
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+
+export const getMyTotalBlogLikes = async(req, res) =>{
+    try {
+        const userId = req.id;
+        const myBlogs = await Blog.find({author:userId}).select("likes")
+        const totalLikes = myBlogs.reduce((acc, blog)=>acc+(blog.likes?.length || 0), 0)
+
+        return res.status(200).json({
+            success:true,
+            totalBlogs:myBlogs.length,
+            totalLikes,
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success:false,
+            message:"Failed to fetch total blog likes"
+        })
+    }
+}
+

1d0c3898e3c28f9b10ccdf4f0b5581cdf2ef542d
Anurag <anurag.2201043cs@iiitbh.ac.in>
2025-06-04 19:01:13 +0530

    backend logic completed

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv"
 import connectDB from "./database/db.js"
 import userRoute from "./routes/user.route.js"
 import blogRoute from "./routes/blog.route.js"
+import commentRoute from "./routes/comment.route.js"
 import cors from "cors"
 import cookieParser from "cookie-parser"
 
@@ -22,6 +23,7 @@ const PORT=process.env.PORT || 3000
 
 app.use("/api/v1/user",userRoute)
 app.use("/api/v1/blog",blogRoute)
+app.use("/api/v1/comment",commentRoute)
 
 
 
